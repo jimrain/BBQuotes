8eb570cda592e81ef74932224a5342b581e0c385
jimrain <jim@rainville.net>
2025-01-26 11:00:21 -0800

    Added fetch functions.

---
+++ BBQuotes/SampleData/Death.swift
@@ -0,0 +1,14 @@
+//
+//  Death.swift
+//  BBQuotes
+//
+//  Created by Jim Rainville on 1/26/25.
+//
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let details: String
+    let lastWords: String 
+}
---
+++ BBQuotes/SampleData/Quote.swift
@@ -0,0 +1,11 @@
+//
+//  Quote.swift
+//  BBQuotes
+//
+//  Created by Jim Rainville on 1/26/25.
+//
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
