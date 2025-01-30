0fa01e850bfca174eed9221a29e7c2757b8c15f4
jimrain <jim@rainville.net>
2025-01-30 09:45:01 -0500

    Version 1 Complete

---
+++ BBQuotes/ViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  ViewModel.swift
+//  BBQuotes
+//
+//  Created by Jim Rainville on 1/26/25.
+//
+import Foundation
+
+// Observable makes this visiable by other views (like @State) and @MainActor makes it the same
+// priority as the UI
+@Observable
+@MainActor
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case fail(error: Error)
+    }
+    
+    // Private(set) makes this variable get-able but not setable
+    private(set) var status: FetchStatus = .notStarted
+    
+    private let fetcher = FetchService()
+    
+    var quote: Quote
+    var character: Char
+    
+    init() {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let quoteData = try! Data(contentsOf: Bundle.main.url(forResource: "samplequote", withExtension: "json")!)
+        quote = try! decoder.decode(Quote.self, from: quoteData)
+        
+        let characterData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
+        character = try! decoder.decode(Char.self, from: characterData)
+    }
+    
+    func getData(for show: String) async {
+        status = .fetching
+        
+        do {
+            quote = try await fetcher.fetchQuote(from: show)
+            character = try await fetcher.fetchCharacter(quote.character)
+            character.death = try await fetcher.fetchDeath(for: character.name)
+            status = .success
+        } catch {
+            status = .fail(error: error)
+        }
+    }
+}
